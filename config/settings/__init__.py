db5ef092d1d53891676808bc77779d0259d17e22
AuroBreeze <1732373074@qq.com>
2025-04-07 11:47:52 +0000

    refactor(project): 重构项目目录结构和基本配置
    - 删除了 .gitignore、Dockerfile、docker-compose.yml 文件
    - 重命名了多个文件和目录，包括 tasks、apps、config 等
    - 新增了 admin、apps、models、tests、views 等文件
    - 更新了 settings、urls、wsgi 等配置文件
    - 调整了 manage.py 文件内容

--- config/settings/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-# 根据环境变量加载不同配置
-env = os.environ.get('DJANGO_ENV', 'development')
-
-if env == 'production':
-    from .production import *
-else:
-    from .development import *
