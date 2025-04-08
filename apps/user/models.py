2f46227222095ccb848063d29fc3e240817e59e9
AuroBreeze <1732373074@qq.com>
2025-04-08 12:49:01 +0000

    feat(user): 为 email 字段添加默认值
    - 在 User_Login 模型中，为 email 字段添加默认值为空字符串
    - 这样可以避免在创建新用户时出现空值错误

--- apps/user/models.py
+++ apps/user/models.py
@@ -7,5 +7,5 @@ class User_Login(models.Model):
     username = models.CharField(max_length=20,validators=[MinLengthValidator(5)],unique=True)
     password = models.CharField(max_length=25)
     join_date = models.DateTimeField(auto_now_add=True)
-    email = models.EmailField(max_length=50,unique=True)
+    email = models.EmailField(max_length=50,unique=True,default='')
     
