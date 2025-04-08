580cfac6cc6e3582fa287ff435dd90f691cda3e9
AuroBreeze <1732373074@qq.com>
2025-04-08 12:48:45 +0000

    refactor(test): 重构测试代码并分离 URL
    - 将单一的 URL 变量拆分为单独的注册和查询 URL 变量
    - 修改查询 URL 的名称，使其更具描述性

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
-url = "http://127.0.0.1:8000/api/v1/user/querr/"
-
-res = requests.post(url).json()
+url_register = "http://127.0.0.1:8000/api/v1/user/register/"
+url_querr = "http://127.0.0.1:8000/api/v1/user/querr/"
+res = requests.post(url_querr).json()
 print(res)
