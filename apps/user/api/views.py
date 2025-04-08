0011d5219d02f1fa2b35ac3a3836bb50efffb071
AuroBreeze <1732373074@qq.com>
2025-04-08 12:49:13 +0000

    refactor(user): 重构用户模块的 API 视图
    - 添加 User_Login 模型导入
    - 实现用户注册功能
    - 优化登录 API
    - 修改 Querr API，使用 ORM 查询用户数据

--- apps/user/api/views.py
+++ apps/user/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db import connection
+from ..models import *
 # Create your views here.
 
 def user_login(request):
@@ -9,18 +10,18 @@ def user_login(request):
 
 class RegisterAPI(APIView):
     def post(self, request):
+        user = User_Login(username="123456", password="1234567890")
+        user.save()
         return Response({"message": "User registered successfully!"})
 
 class LoginAPI(APIView):
     def post(self, request):
+        
         return Response({"message": "User logged in successfully!"})
 
 class Querr(APIView):
     def post(self, request):
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM test_demo")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-        return Response(rows)
-        #return Response({"message": "User logged in successfully!"})
+        users = User_Login.objects.all()
+        users = [user.username for user in users]
+        #print(users)
+        return Response({"message": f"Users:{users}"})
