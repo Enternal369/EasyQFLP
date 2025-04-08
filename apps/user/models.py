30a96ae8097a5faaf73a7f3ba286c14c099e4aab
AuroBreeze <1732373074@qq.com>
2025-04-08 12:19:36 +0000

    feat(user): 添加用户登录模型
    - 新增 User_Login 模型类
    - 包含用户名、密码、注册日期和邮箱字段
    - 用户名长度限制为 5-20 个字符，要求唯一
    - 邮箱地址要求唯一

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class User_Login(models.Model):
+    username = models.CharField(max_length=20,validators=[MinLengthValidator(5)],unique=True)
+    password = models.CharField(max_length=25)
+    join_date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=50,unique=True)
+    
