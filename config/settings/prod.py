28fbca3ca802fe5b7141cb376ee5eb0843e6e4b9
AuroBreeze <1732373074@qq.com>
2025-04-08 17:32:42 +0800

    feat(config): 为生产环境添加 PostgreSQL 数据库配置
    - 在 prod.py 文件中添加了 PostgreSQL 数据库配置
    - 配置包括数据库引擎、名称、用户、密码、主机和端口信息

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,14 @@
 from .base import *
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',  # 确保路径正确
+        'USER': 'postgres',
+        'PASSWORD': 'AuroBreeze',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
