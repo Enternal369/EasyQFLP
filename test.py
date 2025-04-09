0716b65996bd3eba097f728f2dff2a7fcb8259c6
AuroBreeze <1732373074@qq.com>
2025-04-09 17:56:17 +0800

    refactor(test): 修改请求的 URL 地址
    - 将请求的 URL 从 querr 更改为 login
    - 优化代码，提高可读性和维护性

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import requests
 url_register = "http://127.0.0.1:8000/api/v1/user/register/"
 url_querr = "http://127.0.0.1:8000/api/v1/user/querr/"
-res = requests.post(url_querr).json()
+url_login = "http://127.0.0.1:8000/api/v1/user/login/"
+res = requests.post(url_login).json()
 print(res)
