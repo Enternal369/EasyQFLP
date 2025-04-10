1489a419442a913c3196e5da7b9550191dc4c87b
AuroBreeze <1732373074@qq.com>
2025-04-10 13:18:57 +0000

    refactor(user): 优化用户和文章查询接口
    - 在 Querr 类中添加多种用户查询方式的注释示例，包括精确匹配、模糊匹配、范围匹配等
    - 在 ArticleListAPI 类中添加文章筛选示例，展示如何根据关联表进行查询
    - 优化代码注释，提高可读性和可维护性

--- apps/user/api/views.py
+++ apps/user/api/views.py
@@ -35,6 +35,18 @@ class ChangeAPI(APIView):
 class Querr(APIView):
     def post(self, request):
         users = User_Login.objects.all()
+        # users = User_Login.objects.get(username__exact=1) #获取id为1的用户,'__exact'是精确匹配，'__iexact'是忽略大小写的匹配
+        # users = User_Login.objects.filter(username__contains='123').all() #获取用户名包含'123'的用户,'__contains'是包含匹配,'__icontains'是忽略大小写的包含匹配
+        # users = User_Login.objects.filter(username__in=['123','456','789']).all() #获取用户名为'123'、'456'、'789'的用户,'__in'是范围匹配
+        # users = User_Login.objects.filter(username__startswith='123') #获取用户名以'123'开头的用户,'__startswith'是前缀匹配,'__istartswith'是忽略大小写的前缀匹配
+        # users = User_Login.objects.filter(username__endswith='123') #获取用户名以'123'结尾的用户,'__endswith'是后缀匹配,'__iendswith'是忽略大小写的后缀匹配
+        # users = User_Login.objects.filter(join_date__range=('2021-01-01','2021-12-31')) #获取2021年1月1日到2021年12月31日创建的用户,'__range'是范围匹配
+        # users = User_Login.objects.filter(id__gt=1) #获取id大于1的用户,'__gt'是大于匹配,'__gte'是大于等于匹配
+        # users = User_Login.objects.filter(id__lt=1) #获取id小于1的用户,'__lt'是小于匹配,'__lte'是小于等于匹配
+        # users = User_Login.objects.filter(id__isnull=True) #获取id为空的用户,'__isnull'是为空匹配
+        # users = User_Login.objects.filter(username__regex=r'^123') #获取用户名以'123'开头的用户,'__regex'是正则匹配,'__iregex'是忽略大小写的正则匹配
+        
+        # users = User_Login.objects.filter(articles__title__icontains="Article") #使用根据关联表进行查询，获取作者名包含'Article'的文章
         users = [user.username for user in users]
         return Response({"message": f"Users:{users}"})
 
@@ -48,12 +60,15 @@ class ArticleAPI(APIView):
         print(article)
         return Response({"message": f"Article {article.author.username} created successfully!"})
 
-class ArticleListAPI(APIView):
+class ArticleListAPI(APIView): #一对多关系
     def post(self, request):
         user = User_Login.objects.first()
+        #筛选出title包含'Article'的文章
+        #articles = user.articles.filter(title__contains="Article").all()
         articles = user.articles.all() 
         #正常应该是user.article_set.all() 'article'是模型的名字(小写)，加上'_set'固定写法
         #因为Article模型使用了related_name='articles'，所以可以这样写
         #这是两种写法
         articles = [article.title for article in articles]
+        
         return Response({"message": f"Articles:{articles}"})
