b7b65974b08c5ae1f50be274d5109478dffa7891
AuroBreeze <1732373074@qq.com>
2025-04-12 16:25:49 +0800

    refactor(user): 优化用户登录接口
    - 为 email 和 password 字段添加了 required 和 error_messages 参数，提高了数据验证的准确性- 移除了不必要的打印语句和未使用的代码，简化了代码结构
    - 将英文注释和错误消息改为中文，提高了可读性和用户体验

--- apps/user/api/serializers.py
+++ apps/user/api/serializers.py
@@ -6,30 +6,25 @@ from django.core.exceptions import ValidationError
 from ..models import User_Login
 
 class UserLoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
+    email = serializers.EmailField(required=True,error_messages={"required":"邮箱不能为空","invalid":"邮箱格式不正确"})
+    password = serializers.CharField(required=True,write_only=True,error_messages={"required":"密码不能为空","blank":"密码不能为空"})
 
-    def validate(self, data):
+
+    def validate(self, data):#使用中文注释
         email = data.get('email')
         password = data.get('password')
-        print(email,password)
-
+        #print(email,password)
         if email and password:
-            # 只有使用hash保存的密码才能使用authenticate验证
             user = authenticate(email=email, password=password)
-            print(user)
-            # ee = User_Login.objects.get(email=email)
-            # print(ee)
             if user:
                 if user.is_active:
                     data['user'] = user
                 else:
-                    raise ValidationError("User account is disabled.")
+                    raise ValidationError({"ValidationError":"用户已被禁用"})
             else:
-                raise ValidationError("Unable to log in with provided credentials.")
+                raise ValidationError({"ValidationError":"邮箱或密码错误"})
         else:
-            raise ValidationError("Must provide email and password.")
-
+            raise ValidationError({"ValidationError":"邮箱或密码不能为空"})
         return data
 class UserRegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=50,required=True)
